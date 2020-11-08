a3f4efd45f587f99b0b2209b0c32eab8060f08e3
Austin Schrader <austin.jschrader@gmail.com>
2020-11-08 14:50:04 -0800

    Merge sorti s now working
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,38 @@
+// Back End Logic
+function mergeSort(arrayToSort) {
+	if (arrayToSort.length === 1) return arrayToSort;
+	const middleIndex = Math.floor(arrayToSort.length / 2);
+	const firstHalf = mergeSort(arrayToSort.slice(0, middleIndex));
+	const secondHalf = mergeSort(arrayToSort.slice(middleIndex));
+	const sortedArray = [];
+	let i = 0;
+	let j = 0;
+	while (i < firstHalf.length && j < secondHalf.length) {
+		if (firstHalf[i] < secondHalf[j]) {
+			sortedArray.push(firstHalf[i++]);
+		} else {
+			sortedArray.push(secondHalf[j++]);
+		}
+	}
+	while (i < firstHalf.length) sortedArray.push(firstHalf[i++]);
+	while (j < secondHalf.length) sortedArray.push(secondHalf[j++]);
+	return sortedArray;
+}
+
 // Front End Logic
 $(document).ready(function () {
 	$('#formOne').submit(function () {
 		event.preventDefault();
-		let userInput = $('#userInput').val();
-		$('.userResults').append(userInput);
-		numberTo(userInput);
+		// From https://stackoverflow.com/questions/5836833/create-an-array-with-random-values
+		let arrayToSort = Array(25)
+			.fill()
+			.map(() => Math.round(Math.random() * 100));
+
+		// console.log(arrayToSort);
+
+		sortedArray = mergeSort(arrayToSort);
+
+		$('.userResults').append('The unsorted array is: ' + arrayToSort + '</br>');
+		$('.userResults').append('The sorted array is: ' + sortedArray);
 	});
 });
